295079dc35fccd0cf12266418a0a45e12290e73c
ak5k <42914711+ak5k@users.noreply.github.com>
2025-06-02 06:25:43 +0300

    chore: update AppVeyor configuration for Windows ARM64 and adjust CMake output directories

--- cmake/Juce.cmake
+++ cmake/Juce.cmake
@@ -3,6 +3,12 @@ set(target reaper_${PROJECT_NAME})
 
 _juce_initialise_target(${target} ${target})
 
+set(products_folder "${CMAKE_CURRENT_BINARY_DIR}")
+set_target_properties(${target} PROPERTIES
+    ARCHIVE_OUTPUT_DIRECTORY "${products_folder}"
+    LIBRARY_OUTPUT_DIRECTORY "${products_folder}"
+    RUNTIME_OUTPUT_DIRECTORY "${products_folder}")
+
 target_link_libraries(reaper_${PROJECT_NAME}
     PRIVATE
         juce::juce_audio_utils
