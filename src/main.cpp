fc9fbcd302ade13612a0d15bfde09f4fddd27ab2
ak5k <42914711+ak5k@users.noreply.github.com>
2025-06-02 07:42:40 +0300

    feat: add custom action and visibility toggle for main window

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,28 @@
 #include "MainWindow.h"
 MainWindow* mainWindow = nullptr;
 
+int command_id;
+custom_action_register_t custom_action =
+    { //
+        0,
+        "AK5K_HelloWorldDemo",
+        "Hello World Demo",
+        nullptr
+};
+
+bool OnAction(KbdSectionInfo* sec, int command, int val, int valhw, int relmode, HWND hwnd)
+{
+    if (command != command_id)
+        return false;
+
+    if (!mainWindow)
+        return true;
+
+    mainWindow->setVisible(!mainWindow->isVisible());
+
+    return true;
+}
+
 void messagePump()
 {
     juce::MessageManager::getInstance()->runDispatchLoopUntil(10);
@@ -14,14 +36,15 @@ extern "C"
 {
     REAPER_PLUGIN_DLL_EXPORT int REAPER_PLUGIN_ENTRYPOINT(REAPER_PLUGIN_HINSTANCE hInstance, reaper_plugin_info_t* rec)
     {
-        if (rec)
+        if (rec != nullptr && REAPERAPI_LoadAPI(rec->GetFunc) == 0)
         {
-            if (rec->caller_version != REAPER_PLUGIN_VERSION || !rec->GetFunc)
-                return 0;
-
             juce::MessageManager::getInstance()->setCurrentThreadAsMessageThread();
-            rec->Register("timer", (void*)messagePump);
+            plugin_register("timer", (void*)messagePump);
+
             mainWindow = new MainWindow("Hello World Demo", new HelloWorldDemo());
+
+            command_id = plugin_register("custom_action", (void*)&custom_action);
+            plugin_register("hookcommand2", (void*)OnAction);
 
             return 1;
         }
@@ -32,6 +55,8 @@ extern "C"
                 delete mainWindow;
                 mainWindow = nullptr;
             }
+
+            plugin_register("-timer", (void*)messagePump);
 
             juce::shutdownJuce_GUI();
             return 0;
